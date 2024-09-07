93fa3440f7e68254b617af6db41e15b7d08915de
Sadžida Valjevac <valjevac.sadzida02@gmail.com>
2024-09-07 19:59:23 +0200

    updates

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function setMatrix() {
-    let matrix = [["-", "-", "-", "-", "-"],
-                  ["-", "-", "-", "-", "-"],
-                  ["-", "-", "-", "-", "-"],
-                  ["-", "-", "-", "-", "-"],
-                  ["-", "-", "-", "-", "-"]];
+    let matrix = [["?", "?", "?", "?", "?"],
+                  ["?", "?", "?", "?", "?"],
+                  ["?", "?", "?", "?", "?"],
+                  ["?", "?", "?", "?", "?"],
+                  ["?", "?", "?", "?", "?"]];
 
-    let numberSet = 0;
-    let current= 1;
+    let numberSet = 0; //matrix has 0 elements before it is filled
+    let current= 1; //first number/content to start filling your matrix with
 
     while (numberSet < 25) {
-        let x = Math.floor(Math.random()*5);
-        let y = Math.floor(Math.random()*5);
+        let x = Math.floor(Math.random()*5);  //random index to start filling
+        let y = Math.floor(Math.random()*5); //random second index
 
-        if (matrix[x][y] == "-") {
-            matrix[x][y] = current;
-            numberSet++;
+        if (matrix[x][y] == "?") {   //if chosen field has a queastion mark fill it with first content
+            matrix[x][y] = current;  
+            numberSet++;  //increment the number of set fields
 
-            if (numberSet % 2 == 0) {
+            if (numberSet % 2 == 0) {  //to ensure that two fields have the same number, and then changes content to be stored
                 current++;
             }
         }
     }
 
-    for (let i=0; i<5; i++) {
+    for (let i=0; i<5; i++) {        //to put question marks when not opened
         for (let j=0; j<5; j++) {
-            document.getElementById("m" + i + j).innerHTML="-";
+            document.getElementById("m" + i + j).innerHTML="?";
         }
     }
 
@@ -46,31 +46,31 @@ function press(m, n) {
         return;
     }
 
-    if (numOfOpenedFields >= 2) {
+    if (numOfOpenedFields >= 2) {  //if two fields are opened after some time they will close
         return;
     }
 
-    numOfOpenedFields++;
+    numOfOpenedFields++; //1 one field is currently opened
 
-    document.getElementById("m"+ m + n).innerHTML = matrix[m][n];
+    document.getElementById("m"+ m + n).innerHTML = matrix[m][n]; //sets the inner htl of the elemnt of particular ID to the corresponding value in the matrix
 
     if(numOfOpenedFields == 1) {
-        currentOpenedField = {
+        currentOpenedField = {      //if one field is open, it stores cordinates into current opened field variable
             i: m,
             j: n
         };
-    } else if (numOfOpenedFields == 2) {
-        if (matrix[currentOpenedField.i][currentOpenedField.j] == matrix[m][n])  {
-            guessedFields[m][n] = true;
+    } else if (numOfOpenedFields == 2) { //if two fields are opened
+        if (matrix[currentOpenedField.i][currentOpenedField.j] == matrix[m][n])  { //if it is a match
+            guessedFields[m][n] = true; //set it to guessed fields
             guessedFields[currentOpenedField.i][currentOpenedField.j] = true;
-            numOfOpenedFields = 0;
-            document.getElementById("m" + m + n).style.backgroundColor = "#c44b2d";
+            numOfOpenedFields = 0; //restart
+            document.getElementById("m" + m + n).style.backgroundColor = "#c44b2d"; 
             document.getElementById("m"+currentOpenedField.i + currentOpenedField.j).style.backgroundColor = "#c44b2d"
         }
-        else {
-            setTimeout(function () {
-                document.getElementById("m" + m + n).innerHTML = "-"; 
-                document.getElementById("m" + currentOpenedField.i+currentOpenedField.j).innerHTML = "-";
+        else { //if it is not
+            setTimeout(function () { //close both fields
+                document.getElementById("m" + m + n).innerHTML = "?"; 
+                document.getElementById("m" + currentOpenedField.i+currentOpenedField.j).innerHTML = "?";
                 numOfOpenedFields = 0;
             }, 1000);
         }
